bafeb5c4a425eac894e68c0f81ca93a8f6c657e0
dmitrijsbalcers <dmitirjs.balcers@gmail.com>
2015-10-10 18:53:41 +0100

    Add game package, generate cards

---
+++ packages/game/package.js
@@ -0,0 +1,21 @@
+Package.describe({
+  name: 'cah:game',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: '',
+  // URL to the Git repository containing the source code for this package.
+  git: '',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.2.0.2');
+
+  api.use(['reactive-var'], 'client');
+  api.use('cah:common');
+
+  api.addFiles('game.html', 'client');
+  api.addFiles('game.js', 'client');
+});
